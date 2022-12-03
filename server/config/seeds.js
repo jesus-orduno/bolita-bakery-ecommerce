20c7ca51746f8e53ecfc95da4800d5807205dc36
Sharon-Lamanda <lamandakat@yahoo.com>
2022-12-03 15:53:55 -0700

    cookies seeded

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,12 +5,11 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
-  ]);
+    { name: 'Cookies' },
+    { name: 'Muffins' },
+    { name: 'Empanadas' },
+    { name: 'Scones' }
+    ]);
 
   console.log('categories seeded');
 
@@ -18,111 +17,76 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
-      description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+      name: 'Cochitos',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      description:
+        'A staple of any panaderia. A soft and chewy cinnamon gingerbread cookie with a kick of molasses. Packaged in a bag of three mini cochitos.',
+      image: 'bolita-bakery-cochitos.jpg',
+      price: 6.00,
+      quantity: 0
     },
     {
-      name: 'Canned Coffee',
-      description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+      name: 'Cowboy Cookie',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      description:
+        'Six ounce cookie packed with pecans, chocolate chunks, oats, and shredded coconut.',
+      image: 'bolita-bakery-cowboy.webp',
+      price: 6.00,
+      quantity: 0      
     },
     {
-      name: 'Toilet Paper',
+      name: 'Browned Butter Chocolate Chip Cookie',
+      category: categories[0]._id,
+      description:
+        'A classic with a little something extra. Depth of flavor coming from the browned butter and blend of milk and dark chocolate chunks.',
+      image: 'bolita-bakery-choco-chip.jpg',
+      price: 2.00,
+      quantity: 0
+    },
+    {
+      name: 'Agave Dulce de Leche Blondie',
+      category: categories[0]._id,
+      description:
+        'A decadent and chewy blondie with layers of flavor and texture in a 4.5" by 4.5" slice. Browned butter. Hints of agave rum. Swirls of dulce de leche. The crunch of the Lotus Biscoff cookie on top. If you are only getting one thing, this is it.',
+      image: 'bolita-bakery-blondie.webp',
+      price: 10.00,
+      quantity: 0
+    },
+    {
+      name: "S'mores Brownie",
+      category: categories[0]._id,
+      description:
+        'Chewy and fudgy brownie with a graham cracker crust on the bottom and a delicious marshmallow layer on top. A 4.5" x 4.5" slice ready to share or keep all to yourself.',
+      image: 'bolita-bakery-brownie.jpg',
+      price: 12.00,
+      quantity: 0
+    },
+    {
+      name: 'Muffins',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
+        'Coming soon.',
+      // image: 'camera.jpg',
+      // price: 0,
+      quantity: 0
     },
     {
-      name: 'Handmade Soap',
-      category: categories[1]._id,
-      description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
-    },
-    {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
-      name: 'Camera',
+      name: 'Empanadas',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+        'Coming soon.',
+      // image: 'tablet.jpg',
+      // price: 0,
+      quantity: 0
     },
     {
-      name: 'Tablet',
-      category: categories[2]._id,
-      description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
-    },
-    {
-      name: 'Tales at Bedtime',
+      name: 'Scones',
       category: categories[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
-    },
-    {
-      name: 'Spinning Top',
-      category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
-    },
-    {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
+        'Coming soon.',
+      // image: 'bedtime-book.jpg',
+      // price: 0,
+      quantity: 0
     }
   ]);
 
